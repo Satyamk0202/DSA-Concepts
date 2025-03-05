58851fc1d5d5ab5b0f9e8d5385eba1af12ea8b7c
Satyam Kumar <satyakumar9667@gmail.com>
2025-03-05 21:20:02 +0530

    Add files via upload

---
+++ stack/stack.java
@@ -0,0 +1,50 @@
+class Stack {
+    private int arr[];
+    private int top;
+    private int capacity;
+
+    public Stack(int size) {
+        arr = new int[size];
+        capacity = size;
+        top = -1;
+    }
+
+    public void push(int value) {
+        if (top == capacity - 1) {
+            System.out.println("Stack Overflow");
+            return;
+        }
+        arr[++top] = value;
+    }
+
+    public int pop() {
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    public int peek() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return arr[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public int size() {
+        return top + 1;
+    }
+    
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        stack.push(10);
+        stack.push(20);
+        System.out.println(stack.pop()); // 20
+    }
+}
